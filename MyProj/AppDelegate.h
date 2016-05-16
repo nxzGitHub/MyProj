d0d31196616539fa82dab06787d3883cfb353b43
apple <apple@applematoMacBook-Pro.local>
2016-05-16 14:52:33 +0800

    提交说明

---
+++ MyProj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyProj
+//
+//  Created by apple on 16/5/16.
+//  Copyright © 2016年 cn.good. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyProj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyProj
+//
+//  Created by apple on 16/5/16.
+//  Copyright © 2016年 cn.good. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
